e1896a7e01c5e512d0bf99e2e6b935b7e995b9d5
mandhalkaruma <umamandhalkar10@gmail.com>
2026-03-23 14:10:19 +0530

    added matcher

--- sum.test.js
+++ sum.test.js
@@ -8,5 +8,16 @@ describe("test for sum function", () => {
     test("add 1 + 2 is equal 3", () => {
         expect(sum(1, 2)).toBe(3);
     });
-})
+
+    test("array test", () => {
+        expect([1,2,3]).toContain(2);
+    });
+
+    test("object test", () => {
+        expect({a:1}).toEqual({a:1});
+    });
+    test("truthy test", () => {
+        expect(1).toBeTruthy();
+    });
+});
 
